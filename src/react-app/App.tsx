00fd0f6907f5c99d715a1ce3ea15d1f1ddf6c901
Keshav Programmer <amit@pickwinners.io>
2025-03-24 16:05:28 +0530

    no message

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,44 +1,25 @@
 // src/App.tsx
 
-import { useState } from "react";
-import "./App.css";
+import { useEffect } from "react";
+import "./xstream-video-player.css";
+import { XStreamVideoPlayer } from "./xstream-video-player";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("fdsfdsfds");
-
+  let init = false;
+  useEffect(() => {
+    const player = new XStreamVideoPlayer({
+      container: `player`,
+      autoplay: true,
+    });
+    if (!init) {
+      player.init();
+      console.log(player);
+      init = true;
+    }
+  }, [XStreamVideoPlayer]);
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label="increment"
-        >
-          count is {count}
-        </button>
-        <p>
-          Testing Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="card">
-        <button
-          onClick={() => {
-            fetch("/api/")
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name));
-          }}
-          aria-label="get name"
-        >
-          Name from API is: {name}
-        </button>
-        <p>
-          Edit <code>api/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="player"></div>
     </>
   );
 }
