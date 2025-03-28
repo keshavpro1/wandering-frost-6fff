a231eaab5060c1873ead3c1788084637e820dcf1
Keshav Programmer <amit@pickwinners.io>
2025-03-28 09:46:30 +0530

    no message

---
+++ config.js
@@ -0,0 +1,10 @@
+export const streamName = "test";
+// Optional configuration with defaults
+export const videoQuality = 340;
+export const url = "wss://x1.awsmarketplace.services:8443";
+export const autoplay = false;
+
+// Internal state
+export const stream = null;
+export const elements = {};
+export const mouseTimeout = null;
